a89c12e7063f37fadc8cd348d0656a3518f2814e
Faith Analog <faith@faithanalog.com>
2018-12-11 05:59:27 +0000

    Some modifications

--- day11/solutions.c
+++ day11/solutions.c
@@ -1,9 +1,9 @@
+#include <stdio.h>
+
+// Puzzle input
 #define SERIAL_NUMBER 4172
 
 #define GRID_SIZE 300
-
-#include <stdio.h>
-
 
 int powerCache[GRID_SIZE * GRID_SIZE];
 
@@ -20,9 +20,6 @@ int powerLevelCalc(int x, int y) {
 }
 
 int powerLevel(int x, int y) {
-    if (x < 1 || y < 1 || x > GRID_SIZE || y > GRID_SIZE) {
-        return 0;
-    }
     return powerCache[(x - 1) + (y - 1) * GRID_SIZE];
 }
 
@@ -35,7 +32,7 @@ void primeCache() {
     }
 }
 
-int powerSquare(int x, int y, int size) {
+int updatePowerSquare(int x, int y, int size) {
     int p = gridCache[x - 1 + (y - 1) * GRID_SIZE];
 
     for (int xx = x; xx < x + size; xx++) {
@@ -49,49 +46,37 @@ int powerSquare(int x, int y, int size) {
     return p;
 }
 
-void maxPowerSquare(int* gridX, int* gridY, int* power, int size) {
-    int p = *power;
-    int gx = *gridX;
-    int gy = *gridY;
+int maxPowerSquare(int* gridX, int* gridY, int* power, int size) {
+    int increasedMax = 0;
     for (int y = 1; y <= GRID_SIZE - (size - 1); y++) {
         for (int x = 1; x <= GRID_SIZE - (size - 1); x++) {
-            int nextPower = powerSquare(x, y, size);
-            if (nextPower > p) {
-                p = nextPower;
-                gx = x;
-                gy = y;
+            int nextPower = updatePowerSquare(x, y, size);
+            if (nextPower > *power) {
+                *power = nextPower;
+                *gridX = x;
+                *gridY = y;
+                increasedMax = 1;
             }
         }
     }
-    *gridX = gx;
-    *gridY = gy;
-    *power = p;
-}
-
-void maxPowerSquareForever(int* maxX, int* maxY, int* maxSize) {
-    int power = 0;
-    for (int size = 2; size <= GRID_SIZE; size++) {
-        int p = 0;
-        int x = 1;
-        int y = 1;
-        maxPowerSquare(&x, &y, &p, size);
-        if (size == 3) {
-            printf("Part 1: %d,%d\n", x, y);
-        }
-        if (p > power) {
-            power = p;
-            *maxSize= size;
-            *maxX = x;
-            *maxY = y;
-        }
-    }
+    return increasedMax;
 }
 
 int main() {
     int x = 0;
     int y = 0;
-    int size = 0;
+    int maxSize = 0;
+    int power = 0;
     primeCache();
-    maxPowerSquareForever(&x, &y, &size);
-    printf("Part 2: %d,%d,%d\n", x, y, size);
+    
+    // Manually do 2 and 3 to get Part 1
+    maxPowerSquare(&x, &y, &power, 2);
+    maxPowerSquare(&x, &y, &power, 3);
+    printf("Part 1: %d,%d\n", x, y);
+    for (int size = 4; size <= GRID_SIZE; size++) {
+        if (maxPowerSquare(&x, &y, &power, size)) {
+            maxSize = size;
+        }
+    }
+    printf("Part 2: %d,%d,%d\n", x, y, maxSize);
 }
